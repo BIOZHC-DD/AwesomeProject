208a338b2c44dbed16cdcbe8621f69fdfc585139
Tushar Dahiya <shreyashsam@gmail.com>
2024-08-16 17:46:28 +0530

    hwo

--- App.tsx
+++ App.tsx
@@ -24,13 +24,109 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {useState} from 'react';
+import {Button, Image} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import {convertToRGB} from 'react-native-image-to-rgb';
+import * as ImageManipulator from 'expo-image-manipulator';
+import { manipulateAsync } from 'expo-image-manipulator';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
+import {useTensorflowModel} from 'react-native-fast-tflite';
+
 function Section({children, title}: SectionProps): React.JSX.Element {
+  const tf = useTensorflowModel(
+    require('./assets/east-text-detector-tflite-dr-v1.tflite'),
+  );
+  //const car = tf.state;
+  //const ccar = tf.model;
+
+  const car = 'caw';
+  const ccar = 'crow';
+
+  console.log(tf.state);
+  console.log(tf.model);
   const isDarkMode = useColorScheme() === 'dark';
+
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+    //console.log("go goa gone");
+    //console.log(rgbArray);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      //   console.log("go pune puni");
+    }
+    //  const rgbArray = await convertToRGB(result.assets[0].uri);
+    //   console.log(rgbArray);
+
+    //    const resulter = await convertToRGB();
+    //
+    //
+    //
+    //
+
+    const resized = await manipulateAsync(
+                result.assets[0].uri,
+                [{ resize: { height: 320, width: 320 } }],
+                { base64: true }
+            )
+            const convertedArray = await convertToRGB(resized.uri)
+            console.log("convertedArray length");
+            console.log(convertedArray.length);
+            //console.log(convertedArray);
+            const arrayBuffer = new Uint8Array(convertedArray)
+            console.log("arrayBuffer length");
+            console.log(arrayBuffer.length);
+            //console.log(arrayBuffer);
+            const resultt = await tf.model?.runSync([arrayBuffer])
+            //console.log("yeh ai ahia ahiaiaiaa ahia");
+            //console.log(resultt?.length);
+            //console.log(resultt[resultt.length - 1]);
+            //console.log(Array.isArray(resultt));
+            //console.log(typeof resultt);
+            //console.log(Array.isArray(resultt.tensor));
+            //console.log(JSON.stringify(resultt, null, 2));
+            //console.log("jheen tapak dum dum");
+            //console.log(resultt.constructor.name);
+            //
+            //
+
+            //console.log(resultt);
+            
+
+            console.log("resultt length");
+            
+//            console.log(resultt?.length);
+            console.log(resultt[0]?.length);
+            console.log(resultt[1]?.length);
+            //console.log(resultt);
+            //
+            //
+            console.log(resultt[0][0]);
+            console.log(resultt[0][1]);
+            console.log(resultt[0][2]);
+            console.log(resultt[0][3]);
+            console.log(resultt[0][4]);
+            console.log(resultt[0][5]);
+            console.log(resultt[0][6]);
+
+
+     };
+
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -42,6 +138,8 @@ function Section({children, title}: SectionProps): React.JSX.Element {
         ]}>
         {title}
       </Text>
+      <Text>TensorFlow State: {JSON.stringify(car)}</Text>
+      <Text>TensorFlow Model: {JSON.stringify(ccar)}</Text>
       <Text
         style={[
           styles.sectionDescription,
@@ -51,6 +149,10 @@ function Section({children, title}: SectionProps): React.JSX.Element {
         ]}>
         {children}
       </Text>
+      <View style={styles.container}>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && <Image source={{uri: image}} style={styles.image} />}
+      </View>
     </View>
   );
 }
@@ -76,18 +178,8 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
           <Section title="Debug">
             <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
         </View>
@@ -112,6 +204,15 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 });
 
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,23 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+//const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+//
+///**
+// * Metro configuration
+// * https://reactnative.dev/docs/metro
+// *
+// * @type {import('metro-config').MetroConfig}
+// */
+//const config = {};
+//
+//module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+//
+//
+//
+//
+//
+//
+
+const {getDefaultConfig} = require('expo/metro-config');
+const {mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +25,10 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    assetExts: ['tflite', 'png'],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
